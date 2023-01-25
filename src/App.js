8b238311978ab59873c837d830e2ed062bf9cd70
Joseph M <jmedina1985@hotmail.com>
2023-01-25 13:28:31 -0800

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css.bootstrap.min.css';
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Button,
+  Row,
+  Card,
+} from 'react-bootstrap';
 
 function App() {
   return <div className="App"></div>;
