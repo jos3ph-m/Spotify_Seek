aef6c98e8d4dac158cf771b0c76a5e2a84a19d76
Joseph M <jmedina1985@hotmail.com>
2023-01-27 20:59:04 -0800

    Adding rows/grid to the layout

--- src/App.js
+++ src/App.js
@@ -45,6 +45,24 @@ function App() {
               <Card.Title>Album name here</Card.Title>
             </Card.Body>
           </Card>
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album name here</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album name here</Card.Title>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Img src="#" />
+            <Card.Body>
+              <Card.Title>Album name here</Card.Title>
+            </Card.Body>
+          </Card>
         </Row>
       </Container>
     </div>
